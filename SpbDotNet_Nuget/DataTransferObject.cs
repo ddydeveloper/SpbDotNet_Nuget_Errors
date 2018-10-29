b5d17f4ae70e503d356ec1a95654cd006d25dcab
Daniil <ddydeveloper@gmail.com>
2018-10-30 00:09:06 +0300

    Init repo

---
+++ SpbDotNet_Nuget/DataTransferObject.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SpbDotNet_Nuget
+{
+    public class DataTransferObject
+    {
+        public int Id { get; set; }
+        
+        public string Name { get; set; }
+        
+        public string Description { get; set; }
+    }
+}
---
+++ SpbDotNet_Nuget_Error/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using SpbDotNet_Nuget;
+
+namespace SpbDotNet_Nuget_Error
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dto = new DataTransferObject {Id = 1, Name = "First", Description = "Description of the first"};
+            Console.WriteLine($"{dto.Id}-{dto.Name}-{dto.Description}");
+        }
+    }
+}
