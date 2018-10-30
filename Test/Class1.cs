16927dbd14f8a18daa0dcf53c493c7bc1949f97a
Daniil <d.doronkin@itransition.com>
2018-10-30 12:38:39 +0300

    restore from the right feed

---
+++ Test/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Test
+{
+    public class Class1
+    {
+        //private readonly DataTransferObject _dto;
+
+        public Class1()
+        {
+            //_dto = new DataTransferObject();
+        }
+    }
+}
