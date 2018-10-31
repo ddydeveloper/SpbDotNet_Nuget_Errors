a99fca209b5bfe8e4e505a35aeaeb091dc3bfa2e
Daniil <d.doronkin@itransition.com>
2018-10-31 19:52:16 +0300

    Nuget experiments

--- SpbDotNet_Nuget/DataTransferObject.cs
+++ SpbDotNet_Nuget/DataTransferObject.cs
@@ -4,7 +4,7 @@ namespace SpbDotNet_Nuget
 {
     public class DataTransferObject
     {
-        public Guid Id { get; set; }
+        public int Id { get; set; }
         
         public string Name { get; set; }
 
