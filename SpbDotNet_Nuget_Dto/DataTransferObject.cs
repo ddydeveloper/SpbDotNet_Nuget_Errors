68887ca0016bee1f81d260f0a2d011ac09336fa9
Daniil <ddydeveloper@gmail.com>
2018-10-31 23:03:15 +0300

    Changed nuget package

--- SpbDotNet_Nuget_Dto/DataTransferObject.cs
+++ SpbDotNet_Nuget_Dto/DataTransferObject.cs
@@ -1,8 +1,10 @@
-﻿namespace SpbDotNet_Nuget_Dto
+﻿using System;
+
+namespace SpbDotNet_Nuget_Dto
 {
     public class DataTransferObject
     {
-        public int Id { get; set; }
+        public Guid Id { get; set; }
         
         public string Name { get; set; }
 
