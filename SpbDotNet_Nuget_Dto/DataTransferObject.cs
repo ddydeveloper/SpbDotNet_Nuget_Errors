e92cf17d452d3bf9ea2f2def71328f95347df5fc
Daniil <d.doronkin@itransition.com>
2018-11-16 11:54:36 +0300

    commit

--- SpbDotNet_Nuget_Dto/DataTransferObject.cs
+++ SpbDotNet_Nuget_Dto/DataTransferObject.cs
@@ -6,7 +6,7 @@ namespace SpbDotNet_Nuget_Dto
     {
         public int Id { get; set; }
         
-        public string Name { get; set; }
+        public string Name { get; set; } 
 
         public string Description { get; set; }
     }
