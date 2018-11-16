d8264eccd1c4ae59b5d2da1692c978d3dde7b656
Daniil <d.doronkin@itransition.com>
2018-11-16 12:43:00 +0300

    Updated nuget package

--- SpbDotNet_Nuget_Dtos/NugetDto.cs
+++ SpbDotNet_Nuget_Dtos/NugetDto.cs
@@ -1,10 +1,14 @@
-﻿namespace SpbDotNet_Nuget_Dtos
+﻿using System;
+
+namespace SpbDotNet_Nuget_Dtos
 {
     public class NugetDto
     {
-        public int Id { get; set; }
+        public Guid Id { get; set; }
         
-        public string Name { get; set; }
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
 
         public string Description { get; set; }
     }
