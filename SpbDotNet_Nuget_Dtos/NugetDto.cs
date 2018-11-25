2e67146893f6039db5d7c55c34e22f14aadc1b71
Daniil <ddydeveloper@gmail.com>
2018-11-25 13:55:38 +0300

    Initialize nuget

--- SpbDotNet_Nuget_Dtos/NugetDto.cs
+++ SpbDotNet_Nuget_Dtos/NugetDto.cs
@@ -4,11 +4,9 @@ namespace SpbDotNet_Nuget_Dtos
 {
     public class NugetDto
     {
-        public Guid Id { get; set; }
+        public int Id { get; set; }
         
-        public string FirstName { get; set; }
-
-        public string LastName { get; set; }
+        public string Name { get; set; }
 
         public string Description { get; set; }
     }
