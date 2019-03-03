cb97c5e12f822107cc38f012c6a4bf5af3e36988
Daniil <ddydeveloper@gmail.com>
2019-03-04 02:35:18 +0300

    comments

--- SpbDotNet_Nuget_Dtos/NugetDto.cs
+++ SpbDotNet_Nuget_Dtos/NugetDto.cs
@@ -2,19 +2,19 @@
 
 namespace SpbDotNet_Nuget_Dtos
 {
+    public class Person
+    {
+        public string FullName { get; set; }
+
+        public DateTime BirthDate { get; set; }
+    }
+
     //public class Person
     //{
-    //    public string FullName { get; set; }
+    //    public string FirstName { get; set; }
+
+    //    public string LastName { get; set; }
 
     //    public DateTime BirthDate { get; set; }
     //}
-
-    public class Person
-    {
-        public string FirstName { get; set; }
-
-        public string LastName { get; set; }
-        
-        public DateTime BirthDate { get; set; }
-    }
 }
